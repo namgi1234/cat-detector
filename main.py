ff81365cbdf7cbbe25fabdc9a98e55ec13e21235
Andy <75561206+namgi1234@users.noreply.github.com>
2022-10-22 20:51:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ classes = []
 with open("yolo.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
 layer_names = YOLO_net.getLayerNames()
+# output_layers = [layer_names[i[0] - 1] for i in YOLO_net.getUnconnectedOutLayers()]
 output_layers = [layer_names[i - 1] for i in YOLO_net.getUnconnectedOutLayers()]
 
 while True:
@@ -53,10 +54,12 @@ while True:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 5)
             cv2.putText(frame, label, (x, y - 20), cv2.FONT_ITALIC, 0.5, 
             (255, 255, 255), 1)
-            if label == "cat":
-                playsound('sound.wav')
+            
             
     cv2.imshow("YOLOv3", frame)
-
+    if 15 in class_ids:
+        if 39 in class_ids :
+            playsound("sound.wav")
+            print('cat and garbage_bag')
     if cv2.waitKey(100) > 0:
         break
